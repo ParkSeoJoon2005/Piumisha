1b5b153e8be6d27419066241b6ee04121f5e1aa8
Liku cs <83144124+likucs@users.noreply.github.com>
2021-06-29 23:11:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config(object):
-    API_ID = int(6020175)
-    API_HASH = 0fed0266f47e392e71811591be930e94
-    TOKEN = 1797048582:AAEycxgHMhJKs3TXgAUaR9n23Ctbsnv-JmA
+    API_ID = int("6020175")
+    API_HASH = "0fed0266f47e392e71811591be930e94"
+    TOKEN = "1797048582:AAEycxgHMhJKs3TXgAUaR9n23Ctbsnv-JmA"
