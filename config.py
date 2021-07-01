4ff51409c3ea8033b35f06b3b1ee8b4490fdd40a
Liku cs <83144124+likucs@users.noreply.github.com>
2021-07-01 14:23:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,7 @@
+class Config(object):
+    API_ID = int("6020175")
+    API_HASH = "0fed0266f47e392e71811591be930e94"
+    TOKEN = "1797048582:AAEycxgHMhJKs3TXgAUaR9n23Ctbsnv-JmA"
 owner_id = "1340617370"
 bot_token = "1797048582:AAEycxgHMhJKs3TXgAUaR9n23Ctbsnv-JmA"
 bot_id = "1797048582"
