786438e08c3d9deacc167126083db0f0b0e01b0b
Liku cs <83144124+likucs@users.noreply.github.com>
2021-07-01 15:18:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ class Config(object):
     API_HASH = "0fed0266f47e392e71811591be930e94"
     TOKEN = "1797048582:AAEycxgHMhJKs3TXgAUaR9n23Ctbsnv-JmA"
     ARQ_API_KEY = "Get this from @ARQBot"
+    ARQ_API_BASE_URL = "https://thearq.tech"
 owner_id = "1340617370"
 bot_token = "1797048582:AAEycxgHMhJKs3TXgAUaR9n23Ctbsnv-JmA"
 bot_id = "1797048582"
