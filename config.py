d5b0b203e148feebd74bd5c3193086c54966a55e
matheeshaofficial <77558226+matheeshaofficial@users.noreply.github.com>
2021-08-06 18:36:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 class Config(object):
-    API_ID = int("6020175")
-    API_HASH = "0fed0266f47e392e71811591be930e94"
-    TOKEN = "1797048582:AAEycxgHMhJKs3TXgAUaR9n23Ctbsnv-JmA"
-    ARQ_API_KEY = "Get this from @ARQBot"
+    API_ID = int("2885017")
+    API_HASH = "fddc3353cf2cca30aa178fa4725378ed"
+    TOKEN = "1832302598:AAGlabAf6SFSwbhc2T1b4R_0rVVj0Enq4zc"
+    ARQ_API_KEY = "KCDEOJ-TITYTS-CKYUVA-GXOLJO-ARQ"
     ARQ_API_BASE_URL = "https://thearq.tech"
-owner_id = "1340617370"
-bot_token = "1797048582:AAEycxgHMhJKs3TXgAUaR9n23Ctbsnv-JmA"
-bot_id = "1797048582"
+owner_id = "1506444772"
+bot_token = "1832302598:AAGlabAf6SFSwbhc2T1b4R_0rVVj0Enq4zc"
+bot_id = "1832302598"
 
 # Leave it as it is
 ARQ_API_BASE_URL = "https://thearq.tech"
